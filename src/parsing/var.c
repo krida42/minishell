b338e183747e845e99bf690ff0971f00b1ace50e
Kevin Isikayadir <iskrida42@gmail.com>
2022-07-22 04:22:19 +0200

    Parsing of variable good, J'omets l'interpretation des variable quand ils sont suivis de commande manque plus que l'affection dans env

--- src/parsing/var.c
+++ src/parsing/var.c
@@ -1,5 +1,6 @@
 #include "libft.h"
 #include "minishell.h"
+#include <stdio.h>
 
 static int	is_varinit(char *s)
 {
@@ -16,14 +17,45 @@ static int	is_varinit(char *s)
 	return (0);
 }
 
+static	int	are_allvarinit(char **ag)
+{
+	while (*ag)
+	{
+		if (!is_varinit(*ag))
+			return (0);
+		ag++;
+	}
+	return (1);
+}
+
 void	init_allvar(t_cmd *cmd)
 {
 	char	**ag;
+	int		i;
 
 	ag = cmd->ag;
-	while (*ag)
+	i = 0;
+	//Les cas ou l'initialiasaiotn ' de variable est annule
+	if (!are_allvarinit(ag) || cmd->next || cmd->prev)
 	{
-
-		ag++;
+		printf("ag[0]: %s\n", ag[0]);
+		while (ag && ag[i])
+		{
+			printf("i: %d\n", i);
+			if (is_varinit(ag[i]))
+			{
+				printf("INIT\n");
+				strs_remove(&ag, i);
+				i = 0;
+				continue;
+			}
+			i++;
+		}
 	}
+	if (!ag[0])
+	{
+		free_strs(ag);
+		ag = NULL;
+	}
+	cmd->ag = ag;
 }
