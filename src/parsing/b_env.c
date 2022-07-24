cd174b847f02de8c315ad2ed5844345a5e2fe918
Kevin Isikayadir <iskrida42@gmail.com>
2022-07-25 00:47:58 +0200

    save

---
+++ src/parsing/b_env.c
@@ -0,0 +1,10 @@
+#include "minishell.h"
+
+int	b_env(t_info *info, char **args)
+{
+	if (!args)
+		exit(printf(RED"DANGER - b_env() - args is NULL"WHITE));
+	if (!*args)
+		return (e)
+	return (0);
+}
